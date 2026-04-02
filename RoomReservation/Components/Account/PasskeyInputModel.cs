ec342f68202404d99ab84dcf19a104818032e9ef
Argonaut <adakam2267@gmail.com>
2026-04-02 13:06:41 +0200

    Delete RoomReservation directory

--- RoomReservation/Components/Account/PasskeyInputModel.cs
+++
@@ -1,8 +0,0 @@
-namespace RoomReservation.Components.Account
-{
-    public class PasskeyInputModel
-    {
-        public string? CredentialJson { get; set; }
-        public string? Error { get; set; }
-    }
-}
--- RoomReservation/Components/Account/PasskeyOperation.cs
+++
@@ -1,8 +0,0 @@
-namespace RoomReservation.Components.Account
-{
-    public enum PasskeyOperation
-    {
-        Create = 0,
-        Request = 1,
-    }
-}
