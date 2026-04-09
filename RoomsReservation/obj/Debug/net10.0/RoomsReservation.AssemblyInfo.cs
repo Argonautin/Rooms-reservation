55bb0a3318d87a829244cbd4fed363af3fb5f96a
Argonaut <adakam2267@gmail.com>
2026-04-09 13:48:13 +0200

    Floor Plan
    Add Floor plan and editing all rooms

--- RoomsReservation/obj/Debug/net10.0/RoomsReservation.AssemblyInfo.cs
+++ RoomsReservation/obj/Debug/net10.0/RoomsReservation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoomsReservation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a333418f3a3998cb9a5ae9bff4a68f9643539aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2530a7c91bd54a14b96d7c11f18218687895e820")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoomsReservation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoomsReservation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
