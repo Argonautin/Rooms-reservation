97914855348e647eca26b5cd03e468882a4e6bdc
Argonaut <adakam2267@gmail.com>
2026-05-29 16:49:38 +0200

    export pdf
    na canvie2 zrobiony więc trzeba to zmienić bo server wyłącza się przy pobieraniu

--- RoomsReservation/Program.cs
+++ RoomsReservation/Program.cs
@@ -8,8 +8,6 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
-
-
 builder.Services.AddSingleton<AppState>();
 
 // Add services to the container.
@@ -25,6 +23,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
