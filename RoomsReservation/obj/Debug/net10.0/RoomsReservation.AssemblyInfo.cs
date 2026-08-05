d2f6a931ecac42eb15285e63373bb0966145320b
Argonaut <adakam2267@gmail.com>
2026-08-05 20:17:18 +0200

    gitignore

--- RoomsReservation/obj/Debug/net10.0/RoomsReservation.AssemblyInfo.cs
+++ RoomsReservation/obj/Debug/net10.0/RoomsReservation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoomsReservation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c66a105ec677ddeea1e82cce5ff97f0142c4c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbfac91dc405ff4407c9d98ce0a3fc445a835d06")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoomsReservation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoomsReservation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
