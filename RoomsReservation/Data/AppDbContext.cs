0e0d6ad958bfd4f620aa96eac5682bc1b8735765
Argonaut <adakam2267@gmail.com>
2026-09-15 19:54:20 +0200

    Css

--- RoomsReservation/Data/AppDbContext.cs
+++ RoomsReservation/Data/AppDbContext.cs
@@ -7,7 +7,6 @@ namespace RoomsReservation.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        public DbSet<Student> Students => Set<Student>();
         public DbSet<Room> Rooms => Set<Room>();
         public DbSet<User> Users => Set<User>();
         public DbSet<RoomEquipment> RoomEquipments => Set<RoomEquipment>();
--- RoomsReservation/Data/Tables/Student.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RoomsReservation.Data.Tables
-{
-    public class Student
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-    }
-}
